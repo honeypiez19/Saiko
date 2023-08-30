1073bb5046f49a1904f674f455e3be8b678db4c1
Kanyapa <kanyapa.sae@gmail.com>
2023-08-30 09:18:50 +0700

    Update confirm page

--- confirm.php
+++ confirm.php
@@ -11,8 +11,8 @@
 
     <link href="css/boostrap_531.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet" />
-    <script src="js/bootstrap_531.min.js"></script>
     <script src="js/jquery_370.min.js"></script>
+    <script src="js/bootstrap_531.min.js"></script>
 </head>
 
 <body>
@@ -40,44 +40,17 @@
                     <td><?php echo $row['Product_name']; ?></td>
                     <td class="text-center"><?php echo $row['Qty']; ?></td>
                     <td><?php echo $row['Unit']; ?></td>
-                    <td class="text-center"><?php echo $row['Unit_price']; ?></td>
+                    <td class="text-center">
+                        <!--  เมื่อราคาถูกแก้ไขผ่าน Ajax ราคาเปลี่ยนไม่ต้องรีเฟรชหน้าเว็บใหม่ -->
+                        <span class="unit-price"><?php echo $row['Unit_price']; ?></span>
+                    </td>
                     <td class="text-center"><?php echo $row['Residual_value']; ?></td>
                     <td>
                         <form class="form">
                             <input type="hidden" class="product-code" value="<?php echo $row['Product_code']; ?>">
-                            <input type="number" class="price-change" placeholder="ราคา"
-                                value="<?php echo $row['Unit_price']; ?>">
+                            <input type="text" class="price-change" placeholder="ราคา">
                             <button type="button" class="btn btn-warning btn-change-price">แก้ไข</button>
                         </form>
-                        <script type="text/javascript">
-                        $(document).ready(function() {
-                            $(".btn-change-price").click(function() {
-                                var button = $(this);
-                                var form = button.closest("form");
-                                var productCode = form.find(".product-code").val();
-                                var priceChange = form.find(".price-change").val();
-
-                                $.ajax({
-                                    type: "post",
-                                    url: "change.php",
-                                    data: {
-                                        Product_code: productCode,
-                                        price_change: priceChange
-                                    },
-                                    success: function(result) {
-                                        if (result.status == 1) {
-                                            alert(result.message);
-                                            // อัปเดตราคาในตารางหลังจากแก้ไข
-                                            form.closest("tr").find(".text-center").eq(3)
-                                                .text(priceChange);
-                                        } else {
-                                            alert(result.message);
-                                        }
-                                    }
-                                });
-                            });
-                        });
-                        </script>
                     </td>
                 </tr>
                 <?php endwhile?>
@@ -88,6 +61,36 @@
         </table>
         <!-- end table product -->
     </div>
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $(".btn-change-price").click(function() {
+            var button = $(this);
+            var form = button.closest("form");
+            var productCode = form.find(".product-code").val();
+            var priceChange = form.find(".price-change").val();
+
+            $.ajax({
+                type: "post",
+                url: "change.php",
+                data: {
+                    Product_code: productCode,
+                    price_change: priceChange
+                },
+                success: function(result) {
+                    if (result.status == 1) {
+                        alert(result.message);
+                        // อัปเดตราคาในตารางหลังจากแก้ไขและแสดงเป็นทศนิยม 2 ตำแหน่ง
+                        var formattedPrice = parseFloat(priceChange).toFixed(2);
+                        form.closest("tr").find(".unit-price").text(formattedPrice);
+                    } else {
+                        alert(result.message);
+                    }
+                }
+            });
+        });
+    });
+    </script>
 </body>
 
 </html>
